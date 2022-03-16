a8ddae0d8577183083574f8db0ceeb2f71445108
Cozmin <cozmin.ghenea@gmail.com>
2022-03-16 12:17:03 +0200

    Some tries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'https://cozmin-ghenea.github.io/humans_card/index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import App from './containers/App';
+import App from 'https://cozmin-ghenea.github.io/humans_card/containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
